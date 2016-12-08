83dd500097cac524c9add7479bd7bc4fe2d0e057
aymen korkot <ay.korkot@feeling-group.com>
2016-12-08 17:59:07 +0000

    modif1

---
+++ testbd.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+
+</head>
+
+<body>
+
+	<fieldset>
+
+		<legend>Vos coordonnées :</legend>
+		<label for="nom">Nom :</label>
+		<input type="nom" name="nom" size="20" maxlength="40" value="nom" id="nom" />
+		<label for="prenom">prenom :</label>
+		<input type="prenom" name="prenom" size="20" maxlength="40" value="prenom" id="prenom" />
+		<label for="email">Email :</label>
+		<input type="email" name="email" size="20" maxlength="40" value="email" id="email" />
+		<label for="age">Age :</label>
+		<input type="age" name="age" size="20" maxlength="40" value="age" id="age" />
+		<label for="comments">Vos commentaires :</label>
+		<textarea name="comments" id="comments" cols="20" rows="4"></textarea>
+	
+	</fieldset>
+
+	<input type="submit" value="Envoyer" />
+	<input type="reset" value="Annuler" />
+
+
+</form>
+
+
+</body>
+		 
+
+
+</html>
+
+
+
